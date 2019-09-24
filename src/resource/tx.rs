1e2fc8d05ecc6c132cf71c3d6637b4ed92773050
Quentin Boyer <qbsecond@gmail.com>
2019-09-24 14:46:25 +0200

    Things

--- src/resource/tx.rs
+++ src/resource/tx.rs
@@ -2,7 +2,7 @@ use ensicoin_serializer::types::Sha256Result;
 use ensicoin_serializer::{Deserialize, Serialize};
 use sha2::Digest;
 
-use super::script::OP;
+use super::script::{Script, OP};
 use crate::message::{Message, MessageType};
 
 #[derive(Hash, Eq, PartialEq, Clone, Serialize, Deserialize, Debug)]
@@ -40,6 +40,27 @@ impl Transaction {
         hasher = sha2::Sha256::default();
         hasher.input(first);
         hasher.result()
+    }
+
+    pub fn shash(
+        &self,
+        i: usize,
+        referenced_outpoint: &Outpoint,
+        referenced_script: &Script,
+    ) -> Sha256Result {
+        let mut hasher_outpoints = sha2::Sha256::default();
+        for input in &self.inputs {
+            hasher_outpoints.input(input.previous_output.serialize());
+        }
+        let outpoint_simple_hash = hasher_outpoints.result();
+        let mut hasher = sha2::Sha256::default();
+        hasher.input(outpoint_simple_hash);
+        let outpoint_hash = hasher.result();
+
+        let mut hasher = sha2::Sha256::default();
+        hasher.input(self.version.serialize());
+        hasher.input(self.flags.serialize());
+        hasher.input(outpoint_hash);
     }
 }
 
